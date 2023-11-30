80bbf34e4787ee77df264de73d6f6ac065356cfd
gustavo-candidoo <lg0702@hotmail.com>
2023-11-30 16:07:19 -0300

    função para calcular métricas de performance

--- Biblioteca-Gustavo-Candido/gustavocandido.py
+++ Biblioteca-Gustavo-Candido/gustavocandido.py
@@ -1,15 +1,57 @@
 # Tentativa de construir minha própria biblitoeca para automação da construção de modelos de aprendizado de máquina
 
-# Import de bibliotecas iniciais para análise exploratória de dados e análises estatísticas
-def importarBibliotecasEDA():
+
+# Import de bibliotecas
+def importarBibliotecas():
     try:
         import pandas as pd 
         import matplotlib.pyplot as plt
         import seaborn as sns
         import numpy as np 
+        from sklearn.metrics import accuracy_score
+        from sklearn.metrics import roc_auc_score
+        from sklearn.metrics import precision_score
+        from sklearn.metrics import recall_score
+        from sklearn.metrics import f1_score
 
         print("Bibliotecas importadas com sucesso!")
 
     except ImportError as e:
         print(f"Erro ao importar bibliotecas: {e}")
 
+
+# Métricas do modelo
+def calcula_performance(model, x_train, y_train, x_test, y_test):
+
+    ypred_train = model.predict(x_train)
+    ypred_proba_train = model.predict_proba(x_train)[:,1]
+
+    ypred_test = model.predict(x_test)
+    ypred_proba_test = model.predict_proba(x_test)[:,1]
+
+    acc_train = accuracy_score(y_train, ypred_train)
+    acc_test = accuracy_score(y_test, ypred_test)
+    
+    roc_train = roc_auc_score(y_train, ypred_proba_train)
+    roc_test = roc_auc_score(y_test, ypred_proba_test)
+
+    
+    prec_train = precision_score(y_train, ypred_train, zero_division=0)
+    prec_test = precision_score(y_test, ypred_test, zero_division=0)
+    
+    recl_train = recall_score(y_train, ypred_train)
+    recl_test = recall_score(y_test, ypred_test)
+    
+    f1_train = f1_score(y_train, ypred_train)
+    f1_test = f1_score(y_test, ypred_test)
+
+    df_desemp = pd.DataFrame({'Treino':[acc_train, roc_train,
+                                        prec_train, recl_train, f1_train],
+                              'Teste':[acc_test, roc_test, 
+                                       prec_test, recl_test, f1_test]},
+                            index=['Acurácia','AUROC',
+                                   'Precision','Recall','F1'])
+    
+    df_desemp['Variação'] = round(df_desemp['Teste'] / df_desemp['Treino'] - 1, 2)
+    
+    return df_desemp
